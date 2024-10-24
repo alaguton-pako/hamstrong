5b6b4fc8e37dcdab617a25dc91b962cf9582ec42
Ahmed <lameda789@gmail.com>
2024-10-24 14:07:35 +0100

    recent update

--- src/app/components/DrawerComponent.jsx
+++ src/app/components/DrawerComponent.jsx
@@ -12,6 +12,8 @@ import { styled } from "@mui/material/styles";
 import IconButton from "@mui/material/IconButton";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import GetStartedButton from "./buttons/GetStratedButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { Button } from "@mui/material";
 
 export default function DrawerComponent() {
   const [open, setOpen] = React.useState(false);
@@ -29,9 +31,13 @@ export default function DrawerComponent() {
     },
   }));
 
+  const usersFavourite = [
+    { name: "Lekki Golf House", location: "kado, Abuja", price: "40,000,000" },
+  ];
+
   const DrawerList = (
     <Box sx={{ width: 300 }} role="presentation" onClick={toggleDrawer(false)}>
-      <div className="h-screen flex justify-center items-center">
+      {/* <div className="h-screen flex justify-center items-center">
         <div className="w-full h-full my-10 flex justify-center items-center">
           <div>
             <p className="text-sm">Nothing to show at the moment</p>
@@ -40,11 +46,38 @@ export default function DrawerComponent() {
             </div>
           </div>
         </div>
+      </div> */}
+      <div className="flex flex-col gap-2">
+        <div className="my-2">
+          <h1 className="text-center text-xl md:text-2xl font-bold text-[#404b82] py-4 px-6 bg-white border-b-4 border-[#404b82] shadow-lg rounded-lg tracking-wide">
+            My Favorites <FavoriteIcon />
+          </h1>
+        </div>
+        <div className="p-2">
+          <List>
+            {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+        </div>
+        <div className="flex justify-center items-center">
+          <Button
+            size="small"
+            variant="contained"
+            sx={{ textTransform: "initial", bgcolor: "#404b82" }}
+          >
+            clear all
+          </Button>
+        </div>
       </div>
-      {/* <List>Ahmeds House</List>
-      <Divider />
-      <List>Kunle House</List>
-      <Divider /> */}
     </Box>
   );
 
@@ -52,7 +85,7 @@ export default function DrawerComponent() {
     <div>
       <IconButton onClick={toggleDrawer(true)}>
         <StyledBadge
-          badgeContent={4}
+          badgeContent={1}
           color="success"
           anchorOrigin={{
             vertical: "top",
